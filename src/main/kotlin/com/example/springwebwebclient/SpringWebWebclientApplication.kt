7ba6933a2b85701d741bb25be73b3b881ad35f18
Renato Costa <renato.rezende.costa@gmail.com>
2021-10-18 19:55:43 +0200

    add sample controller, trace helper function

--- src/main/kotlin/com/example/springwebwebclient/SpringWebWebclientApplication.kt
+++ src/main/kotlin/com/example/springwebwebclient/SpringWebWebclientApplication.kt
@@ -1,11 +1,32 @@
 package com.example.springwebwebclient
 
+import kotlinx.coroutines.delay
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class SpringWebWebclientApplication
 
 fun main(args: Array<String>) {
 	runApplication<SpringWebWebclientApplication>(*args)
+}
+
+@RestController
+class MyHelloController {
+
+	@GetMapping("/hello")
+	fun helloThere() {
+		trace("Hello there!")
+		Thread.sleep(5_000)
+		trace("General Kenobi!")
+	}
+}
+
+/**
+ * Simple console out, but appending the thread name to the message
+ */
+fun trace(msg: Any) {
+	println("[${Thread.currentThread().name}] $msg")
 }
