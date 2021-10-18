2c248d00ea222a871d12510f8436003d96188447
Renato Costa <renato.rezende.costa@gmail.com>
2021-10-18 19:49:09 +0200

    initial commit

---
+++ src/main/kotlin/com/example/springwebwebclient/SpringWebWebclientApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springwebwebclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebWebclientApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebWebclientApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springwebwebclient/SpringWebWebclientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springwebwebclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebWebclientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
