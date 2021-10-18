831333cd9d19134350a59258546042d5f42ce040
Renato Costa <renato.rezende.costa@gmail.com>
2021-10-18 20:29:36 +0200

    implement resttemplate http request to delay service

--- src/main/kotlin/com/example/springwebwebclient/SpringWebWebclientApplication.kt
+++ src/main/kotlin/com/example/springwebwebclient/SpringWebWebclientApplication.kt
@@ -1,32 +1,60 @@
 package com.example.springwebwebclient
 
-import kotlinx.coroutines.delay
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForObject
+import java.util.UUID
 
 @SpringBootApplication
 class SpringWebWebclientApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringWebWebclientApplication>(*args)
+    runApplication<SpringWebWebclientApplication>(*args)
 }
 
 @RestController
-class MyHelloController {
+class MyHelloController(
+    private val delayService: DelayService,
+) {
+    @GetMapping("/hello")
+    suspend fun helloThere() {
+        trace("Hello there!")
+        delayService.delay()
+        trace("General Kenobi")
+    }
+}
 
-	@GetMapping("/hello")
-	suspend fun helloThere() {
-		trace("Hello there!")
-		delay(5_000)
-		trace("General Kenobi!")
-	}
+@Service
+class DelayService(
+    private val restTemplate: RestTemplate,
+) {
+    suspend fun delay() {
+        restTemplate.getForObject<Unit>(url = "$ENDPOINT_URL/$DELAY_SECONDS")
+    }
+
+    companion object {
+        private const val ENDPOINT_URL = "https://httpbin.org/delay"
+        private const val DELAY_SECONDS = 5
+    }
+}
+
+data class UUIDServiceResponse(val uuid: UUID)
+
+@Configuration
+class HttpClientConfiguration {
+    @Bean
+    fun restTemplate(): RestTemplate = RestTemplate()
 }
 
 /**
  * Simple console out, but appending the thread name to the message
  */
 fun trace(msg: Any) {
-	println("[${Thread.currentThread().name}] $msg")
+    println("[${Thread.currentThread().name}] $msg")
 }
